8b0b7014e81d136240654f19d5ef61bc6b734acf
Sam Gielis <sam.gielis@trustedfamily.net>
2021-11-25 22:09:55 +0100

    Don't load iframe unless in view
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/components/Walk/index.tsx
+++ src/components/Walk/index.tsx
@@ -1,6 +1,7 @@
 import { Center, Heading, Stack } from "@chakra-ui/layout";
 import { Skeleton } from "@chakra-ui/skeleton";
 import { useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 interface WalkProps {
     komootId?: string;
@@ -9,12 +10,17 @@ interface WalkProps {
 
 export const Walk = ({ komootId, location }: WalkProps) => {
     const [isLoaded, setLoaded] = useState(false);
-    return <Stack spacing={3} fontFamily="sans-serif">
+    const { ref, inView, entry } = useInView({
+        /* Optional options */
+        threshold: 0,
+      });
+      
+    return <Stack spacing={3} fontFamily="sans-serif" ref={ref}>
             <Heading as="h2" textTransform="uppercase" color="rgb(79, 133, 13)">
                 {location}
             </Heading>
         {komootId && <Skeleton isLoaded={isLoaded}>
-            <iframe onLoad={() => { setLoaded(true) }} src={`https://www.komoot.com/tour/${komootId}/embed`} width="100%" height="500px" frameBorder="0" scrolling="no"></iframe>
+            {(inView || isLoaded) && <iframe onLoad={() => { setLoaded(true) }} src={`https://www.komoot.com/tour/${komootId}/embed`} width="100%" height="500px" frameBorder="0" scrolling="no"></iframe>}
         </Skeleton>
         }
     </Stack>
