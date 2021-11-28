bf7b8bb4e0297ce3c20a79d2a10104458b789a05
Sam Gielis <sam.gielis@trustedfamily.net>
2021-11-28 18:41:20 +0100

    Don't open newsletter popover after first time
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/components/NewsLetterButton/index.tsx
+++ src/components/NewsLetterButton/index.tsx
@@ -4,17 +4,48 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBullhorn } from '@fortawesome/free-solid-svg-icons'
 import { useDisclosure } from "@chakra-ui/hooks"
 import { NewsLetterModal } from "../NewsLetterModal"
+import { Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverCloseButton, PopoverHeader, PopoverBody } from "@chakra-ui/popover"
+import { useState } from "react"
+import { Box } from "@chakra-ui/layout"
+import { useColorModeValue } from "@chakra-ui/color-mode"
+import { useLocalStorage } from "../../hooks/useLocalStorage"
 
 export const NewsLetterButton: React.FC = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const [hasSeenNewsLetterPopover, setHasSeenNewsLetterPopover] = useLocalStorage('hasSeenNewsLetterPopover', false)
+    const [isCTAOpen, setIsOpen] = useState(false);
 
+    const popoverBg = useColorModeValue("gray.800", "gray.100");
+    const popoverColor = useColorModeValue("white", "gray.900");
+
+    React.useEffect(() => {
+        if (hasSeenNewsLetterPopover) {
+            return;
+        }
+
+        setTimeout(() => {
+            setIsOpen(true);
+            setHasSeenNewsLetterPopover(true);
+        }, 3000)
+    }, []);
+    
     return (
         <>
-            <HeaderIconButton
-                onClick={onOpen}
-                icon={<FontAwesomeIcon icon={faBullhorn} />}
-                aria-label={`Inschrijven op nieuwsbrief`}
-            />
+            <Popover isOpen={isCTAOpen} onClose={() => { setIsOpen(false) }} placement="bottom">
+                <PopoverTrigger>
+                    <Box>
+                        <HeaderIconButton
+                            onClick={onOpen}
+                            icon={<FontAwesomeIcon icon={faBullhorn} />}
+                            aria-label={`Inschrijven op nieuwsbrief`}
+                        />
+                    </Box>
+                </PopoverTrigger>
+                <PopoverContent mx={2} bg={popoverBg} color={popoverColor} boxShadow="lg" onClick={onOpen}>
+                    <PopoverArrow bg={popoverBg} color={popoverColor} />
+                    <PopoverBody>Schrijf je in op onze nieuwsbrief!</PopoverBody>
+                </PopoverContent>
+            </Popover>
             <NewsLetterModal isOpen={isOpen} onClose={onClose} />
         </>
     )
