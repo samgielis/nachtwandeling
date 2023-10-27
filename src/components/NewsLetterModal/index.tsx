dbfd454dda4b8f65db9eccfd4291aebafba92584
Sam Gielis <samgielis@gmail.com>
2023-10-27 19:02:28 +0200

    Add #8

--- src/components/NewsLetterModal/index.tsx
+++ src/components/NewsLetterModal/index.tsx
@@ -1,104 +1,131 @@
-import { FormControl, FormLabel, FormErrorMessage, FormHelperText } from "@chakra-ui/form-control"
-import { Input } from "@chakra-ui/input"
-import { Stack } from "@chakra-ui/layout"
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter } from "@chakra-ui/modal"
-import { Button, useToast } from "@chakra-ui/react"
-import { useForm } from "react-hook-form"
+import {
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+} from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { Stack } from "@chakra-ui/layout";
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/modal";
+import { Button, useToast } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 
-const SUBSCRIBE_NEWSLETTER_ENDPOINT = '/.netlify/functions/subscribeToNewsletter';
+const SUBSCRIBE_NEWSLETTER_ENDPOINT =
+  "/.netlify/functions/subscribeToNewsletter";
 
 interface NewsLetterModalProps {
-    isOpen: boolean;
-    onClose(): void;
+  isOpen: boolean;
+  onClose(): void;
 }
 
-export const NewsLetterModal = ({isOpen, onClose}: NewsLetterModalProps) => {
-    const toast = useToast();
-    const {
-        handleSubmit,
-        register,
-        formState: { errors, isSubmitting }
-    } = useForm();
+export const NewsLetterModal = ({ isOpen, onClose }: NewsLetterModalProps) => {
+  const toast = useToast();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm();
 
-    function onSubmit(values: { email: string, name: string }) {
-        const handleError = () => {
+  function onSubmit(values: { email: string; name: string }): Promise<void> {
+    const handleError = () => {
+      toast({
+        title: "Er ging iets mis.",
+        description:
+          "Probeer het later nog eens of contacteer ons op info@n8wndlng.be.",
+        status: "error",
+        isClosable: true,
+      });
+    };
+    return new Promise((resolve, reject) => {
+      fetch(SUBSCRIBE_NEWSLETTER_ENDPOINT, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      })
+        .then((response) => {
+          if (response.status !== 200) {
+            handleError();
+            reject();
+          } else {
             toast({
-                title: "Er ging iets mis.",
-                description: "Probeer het later nog eens of contacteer ons op info@n8wndlng.be.",
-                status: "error",
-                isClosable: true,
+              title: "Ingeschreven.",
+              description: "Je bent succesvol ingeschreven op de nieuwsbrief.",
+              status: "success",
+              isClosable: true,
             });
-        }
-        return new Promise((resolve, reject) => {
-            fetch(SUBSCRIBE_NEWSLETTER_ENDPOINT, {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(values)
-            }).then((response) => {
-                if (response.status !== 200) {
-                    handleError();
-                    reject();
-                } else {
-                    toast({
-                        title: "Ingeschreven.",
-                        description: "Je bent succesvol ingeschreven op de nieuwsbrief.",
-                        status: "success",
-                        isClosable: true,
-                    });
-                    onClose();
-                    resolve(undefined);
-                }
-            }).catch((e) => {
-                handleError();
-                reject();
-            })
+            onClose();
+            resolve(undefined);
+          }
+        })
+        .catch((e) => {
+          handleError();
+          reject();
         });
-    }
+    });
+  }
 
-    return <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <ModalHeader>Schrijf je in op de nieuwsbrief</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody pb={6}>
-                    <Stack>
-                        <FormControl id="email" isRequired>
-                            <FormLabel>E-mail</FormLabel>
-                            <Input type="email"
-                                placeholder="voorbeeld@gmail.com"
-                                {...register("email", {
-                                    required: "E-mail is verplicht",
-                                })} />
-                            <FormErrorMessage>
-                                {errors.email && errors.email.message}
-                            </FormErrorMessage>
-                            <FormHelperText>We gebruiken je e-mailadres enkel om updates te geven over onze wandelingen.</FormHelperText>
+  return (
+    <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <form onSubmit={handleSubmit(onSubmit as any)}>
+          <ModalHeader>Schrijf je in op de nieuwsbrief</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <Stack>
+              <FormControl id="email" isRequired>
+                <FormLabel>E-mail</FormLabel>
+                <Input
+                  type="email"
+                  placeholder="voorbeeld@gmail.com"
+                  {...register("email", {
+                    required: "E-mail is verplicht",
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.email && errors.email.message}
+                </FormErrorMessage>
+                <FormHelperText>
+                  We gebruiken je e-mailadres enkel om updates te geven over
+                  onze wandelingen.
+                </FormHelperText>
+              </FormControl>
+              <FormControl id="name">
+                <FormLabel>Naam</FormLabel>
+                <Input type="text" placeholder="name" {...register("name")} />
+                <FormErrorMessage>
+                  {errors.name && errors.name.message}
+                </FormErrorMessage>
+                <FormHelperText>
+                  Hiermee personaliseren we onze e-mails.
+                </FormHelperText>
+              </FormControl>
+            </Stack>
+          </ModalBody>
 
-                        </FormControl>
-                        <FormControl id="name">
-                            <FormLabel>Naam</FormLabel>
-                            <Input type="text"
-                                placeholder="name"
-                                {...register("name")}
-                            />
-                            <FormErrorMessage>
-                                {errors.name && errors.name.message}
-                            </FormErrorMessage>
-                            <FormHelperText>Hiermee personaliseren we onze e-mails.</FormHelperText>
-                        </FormControl>
-                    </Stack>
-                </ModalBody>
-
-                <ModalFooter>
-                    <Button colorScheme="green" mr={3} isLoading={isSubmitting} type="submit">
-                        Inschrijven
-                    </Button>
-                    <Button onClick={onClose}>Annuleren</Button>
-                </ModalFooter>
-            </form>
-        </ModalContent>
+          <ModalFooter>
+            <Button
+              colorScheme="green"
+              mr={3}
+              isLoading={isSubmitting}
+              type="submit"
+            >
+              Inschrijven
+            </Button>
+            <Button onClick={onClose}>Annuleren</Button>
+          </ModalFooter>
+        </form>
+      </ModalContent>
     </Modal>
-}
+  );
+};
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,34 +1,73 @@
-import { Box, Heading, VStack, Stack, Center, Divider } from "@chakra-ui/layout"
-import { Image, useColorModeValue } from "@chakra-ui/react"
-import { FaMoon } from "react-icons/fa"
-import { Walk } from "../components/Walk"
+import {
+  Box,
+  Center,
+  Divider,
+  Heading,
+  Stack,
+  VStack,
+} from "@chakra-ui/layout";
+import { Image, useColorModeValue } from "@chakra-ui/react";
+import { FaMoon } from "react-icons/fa";
+import { Walk } from "../components/Walk";
 
 export const Home = () => {
-    const filter = useColorModeValue("none", "invert()")
-    return <VStack w="100%" spacing={{base: 6, md: 12}}>
-        <Box w="xs" px={{base: 10, md: 2}}>
-            <Image style={{ filter }} src="https://cdn.pixabay.com/photo/2021/04/05/14/52/hiking-6153739_1280.png" />
-        </Box>
-        <Heading as="h1" size="2xl" textTransform="uppercase">
-            Wandelingen
-        </Heading>
-        <Stack w="100%" divider={<MoonDivider/>}>
-            {/*<Walk location="Averbode bos & heide" komootId="570508073" date={new Date("12/18/2021")} isDraft />*/}
-            <Walk location="Expeditie Glühwein" komootId="986841721" date={new Date("12/17/2022")} />
-            <Walk location="Schaffen Hees (afterparty met vuurschaal)" komootId="924497918" date={new Date("08/10/2022")} />
-            <Walk location="Fort leopold & Dassenaarde" komootId="658668826" date={new Date("02/26/2022")} />
-            <Walk location="Kaggevinne" komootId="570502619" date={new Date("11/19/2021")} />
-            <Walk location="Gerhagen" komootId="503252998" date={new Date("09/25/2021")} />
-        </Stack>
+  const filter = useColorModeValue("none", "invert()");
+  return (
+    <VStack w="100%" spacing={{ base: 6, md: 12 }}>
+      <Box w="xs" px={{ base: 10, md: 2 }}>
+        <Image
+          style={{ filter }}
+          src="https://cdn.pixabay.com/photo/2021/04/05/14/52/hiking-6153739_1280.png"
+        />
+      </Box>
+      <Heading as="h1" size="2xl" textTransform="uppercase">
+        Wandelingen
+      </Heading>
+      <Stack w="100%" divider={<MoonDivider />}>
+        {/*<Walk location="Averbode bos & heide" komootId="570508073" date={new Date("12/18/2021")} isDraft />*/}
+        <Walk
+          location="Slachtwandeling"
+          komootId="1360258523"
+          date={new Date("10/28/2023")}
+        />
+        <Walk
+          location="Expeditie Glühwein"
+          komootId="986841721"
+          date={new Date("12/17/2022")}
+        />
+        <Walk
+          location="Schaffen Hees (afterparty met vuurschaal)"
+          komootId="924497918"
+          date={new Date("08/10/2022")}
+        />
+        <Walk
+          location="Fort leopold & Dassenaarde"
+          komootId="658668826"
+          date={new Date("02/26/2022")}
+        />
+        <Walk
+          location="Kaggevinne"
+          komootId="570502619"
+          date={new Date("11/19/2021")}
+        />
+        <Walk
+          location="Gerhagen"
+          komootId="503252998"
+          date={new Date("09/25/2021")}
+        />
+      </Stack>
     </VStack>
-}
+  );
+};
 
 const MoonDivider = () => {
-    return    <Center my={20}>
-        <Divider/>
-        <Box px={3}>
+  return (
+    <Center my={20}>
+      <Divider />
+      <Box px={3}>
         <FaMoon color="gray.500" />
-            </Box>
-        <Divider/>
+      </Box>
+      <Divider />
     </Center>
-}
+  );
+};
